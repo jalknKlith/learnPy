793c7c13e5163b95a57988abe518a2c596edbf11
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-05-05 19:42:41 -0500

    Add files via upload
    the greatest number between two numbers

---
+++ theGreatestNumber.py
@@ -0,0 +1,8 @@
+a = int(input("write a number "))
+b = int(input("write a number "))
+if (a > b):
+	print("number", a, "is greater than", b)
+elif (a < b):
+	print("number", b, "is greater than", a)
+else:
+	print("the numbers are equal")
